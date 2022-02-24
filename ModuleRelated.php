58b2b03e5cdffcd20c33f85c22476280569cf101
sandygoreraza <gorerazasandy10@gmail.com>
2022-02-24 15:56:34 +0400

    Update ModuleRelated.php

--- ModuleRelated.php
+++ ModuleRelated.php
@@ -1,13 +1,12 @@
 <?php
-/***********************************************************************************************
-** The contents of this file are subject to the Vtiger Module-Builder License Version 1.3
- * ( "License" ); You may not use this file except in compliance with the License
- * The Original Code is:  Technokrafts Labs Pvt Ltd
- * The Initial Developer of the Original Code is Technokrafts Labs Pvt Ltd.
- * Portions created by Technokrafts Labs Pvt Ltd are Copyright ( C ) Technokrafts Labs Pvt Ltd.
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
-**
-*************************************************************************************************/
+ ************************************************************************************/
 
 include_once('vtlib/Vtiger/Module.php');
 
