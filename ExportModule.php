b28d97c3424864133dd82195aa9d32bf6c05f103
sandygoreraza <gorerazasandy10@gmail.com>
2022-02-24 15:50:45 +0400

    Update ExportModule.php

--- ExportModule.php
+++ ExportModule.php
@@ -1,4 +1,13 @@
 <?php
+/***********************************************************************************************
+** The contents of this file are subject to the Vtiger Module-Builder License Version 1.3
+ * ( "License" ); You may not use this file except in compliance with the License
+ * The Original Code is:  Technokrafts Labs Pvt Ltd
+ * The Initial Developer of the Original Code is Technokrafts Labs Pvt Ltd.
+ * Portions created by Technokrafts Labs Pvt Ltd are Copyright ( C ) Technokrafts Labs Pvt Ltd.
+ * All Rights Reserved.
+**
+*************************************************************************************************/
 include_once 'vtlib/Vtiger/Module.php';
 
 $Vtiger_Utils_Log = true;
