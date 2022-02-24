9190522c2543dcdfab584e461a28e7f216508f9d
sandygoreraza <gorerazasandy10@gmail.com>
2022-02-24 15:46:49 +0400

    Update ModuleCreatorVitger.php

--- ModuleCreatorVitger.php
+++ ModuleCreatorVitger.php
@@ -1,4 +1,14 @@
 <?php
+/***********************************************************************************************
+** The contents of this file are subject to the Vtiger Module-Builder License Version 1.3
+ * ( "License" ); You may not use this file except in compliance with the License
+ * The Original Code is:  Technokrafts Labs Pvt Ltd
+ * The Initial Developer of the Original Code is Technokrafts Labs Pvt Ltd.
+ * Portions created by Technokrafts Labs Pvt Ltd are Copyright ( C ) Technokrafts Labs Pvt Ltd.
+ * All Rights Reserved.
+**
+*************************************************************************************************/
+
 /**
  * CREATED BY SANDY GORERAZA USING PHPSTORM 2021.3.2
  * Below is a custom Library for creating Entity Module for Vtiger Framework
